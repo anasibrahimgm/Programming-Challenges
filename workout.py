b98936c0f14267170f6fe8b138293c1ec0f59bcd
anasibrahimgm <anasibrahimgm@gmail.com>
2018-07-23 23:30:11 +0200

    workout-02

--- workout.py
+++ workout.py
@@ -1,10 +1,33 @@
+def double_digits(y):
+	return '0' + str(y) if y < 10 else y
+
+def time_format(seconds):
+	minutes = int(seconds/60)
+	return '({}:{})'.format(double_digits(minutes), double_digits(int(seconds - minutes * 60)))
+
 goal = int(input("Goal in minutes: ")) * 60
 play_period = int(input("Play Period: "))
 pause_period = int(input("Pause Period: "))
 
+print("*******************")
+
 total_time = goal/play_period
+num_doubles = goal/(play_period * 2)
+ratio = pause_period/play_period
 
-total_time_min = (int(total_time) * (play_period + pause_period) + play_period * (total_time - int(total_time)))/60
-total_time_sec = int((total_time_min - int(total_time_min)) * 60)
+rem_time = goal - int(num_doubles) * (play_period * 2)
 
-print("Total Time: ", int(total_time_min), ":", str(total_time_sec) + '0' if total_time_sec < 10 else total_time_sec, sep="")
+total_work_out = int(num_doubles) * 2 * (play_period + pause_period)
+
+print('{}x {}+{}'.format(int(num_doubles) * 2, play_period, pause_period) + time_format(total_work_out))
+
+if rem_time:
+	x = int(rem_time/2)
+	total_work_out += rem_time
+	z = '2x {}'.format(x)
+
+	if rem_time > play_period:
+		total_work_out += x * ratio * 2
+		z += '+{}'.format(int(x * ratio))
+
+	print(z+ time_format(total_work_out))
