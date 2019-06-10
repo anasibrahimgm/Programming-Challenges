915f1999b9deda7928cc1e931e33ebee6f33b0ff
Anas Ibrahim <anasibrahimgm@gmail.com>
2019-06-10 20:21:45 +0200

    9A.py take 03

--- CodeForces/9/9A.py
+++ CodeForces/9/9A.py
@@ -1,7 +1,7 @@
 x = list(map(int, input().split()))
 # She wins if she gets a number equal to or higher than the highest number they've got
-prob = (6 - max(x) + 1) / 6
 
-probs = {1/6: '1/6', 1/3: '1/3', 1/2: '1/2', 2/3: '2/3', 5/6: '5/6', 1: '1/1'}
+# create probs dict depending on max
+probs = {6: '1/6', 5: '1/3', 4: '1/2', 3: '2/3', 2: '5/6', 1: '1/1'}
 
-print(probs[prob])
+print(probs[max(x)])
