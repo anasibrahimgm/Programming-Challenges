25cd2afae480eb73401926eaa348f7e31f1e2cef
Anas Ibrahim <anasibrahimgm@gmail.com>
2019-06-10 20:18:58 +0200

    9A.py take 02

--- CodeForces/9/9A.py
+++ CodeForces/9/9A.py
@@ -4,7 +4,4 @@ prob = (6 - max(x) + 1) / 6
 
 probs = {1/6: '1/6', 1/3: '1/3', 1/2: '1/2', 2/3: '2/3', 5/6: '5/6', 1: '1/1'}
 
-for i in probs:
-    if prob == i:
-        print(probs[i])
-        break
+print(probs[prob])
