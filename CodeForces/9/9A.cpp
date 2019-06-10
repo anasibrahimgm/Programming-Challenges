3c7a0e6e5ce4b265e5a8ec99ad108df686a49f46
Anas Ibrahim <anasibrahimgm@gmail.com>
2019-06-10 20:49:12 +0200

    9A.cpp take 02

--- CodeForces/9/9A.cpp
+++ CodeForces/9/9A.cpp
@@ -9,33 +9,9 @@ int main(int argc, char const* argv[])
 
   int max = x >= y ? x : y;
 
-  switch(max) {
-    case 1:
-      cout << "1/1";
-      break;
+  const char *probs[6] = {"1/1", "5/6", "2/3", "1/2", "1/3", "1/6"};
 
-    case 2:
-      cout << "5/6";
-      break;
-
-    case 3:
-      cout << "2/3";
-      break;
-
-    case 4:
-      cout << "1/2";
-      break;
-
-    case 5:
-      cout << "1/3";
-      break;
-
-    case 6:
-      cout << "1/6";
-      break;
-  }
-
-  cout << endl;
+  cout << probs[max - 1] << endl;
 }
 
 /*
